b977efb66f2d0b539bd433e80762737925035edc
kjvdheide <kjvanderheide@dundee.ac.uk>
2017-11-13 14:58:19 +0000

    JAL-2797 small start on creating either JFrame or JInternalFrame

---
+++ forester/java/src/org/forester/archaeopteryx/AptxLauncher.java
@@ -0,0 +1,11 @@
+package org.forester.archaeopteryx;
+
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+
+public class AptxLauncher {
+    private JFrame aptxStandalone = new JFrame();
+    private JInternalFrame aptxEmbedded = new JInternalFrame();
+    
+    
+}
