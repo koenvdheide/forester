1ac36b76bb69bb061cbd15ee12be6c26a1aaf77b
kjvdheide <kjvanderheide@dundee.ac.uk>
2018-02-10 18:33:55 +0100

    JAL-2797 added adapter for the dummy frame listener

---
+++ forester/java/src/org/forester/archaeopteryx/FrameAdapter.java
@@ -0,0 +1,52 @@
+package org.forester.archaeopteryx;
+
+import java.awt.event.WindowEvent;
+
+import javax.swing.event.InternalFrameEvent;
+
+public class FrameAdapter implements FrameListener {
+
+    @Override
+    public void FrameActivated() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void FrameClosed() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void FrameClosing() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void FrameDeactivated() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void FrameDeiconified() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void FrameIconified() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void FrameOpened() {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+}
